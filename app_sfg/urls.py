f222bcd8c00c8990ca9904869d54175a617921bc
TanabordeeCj <tanabordee0313@gmail.com>
2024-09-11 19:04:18 +0700

    update

--- app_sfg/urls.py
+++ app_sfg/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-form . import views
+from . import views
 
 urlpatterns = [
-    path("", views.IndexPage, name="index")
+    path("", views.IndexPage, name="index"),
 ]
--- main_sfg/urls.py
+++ main_sfg/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("admin/", include('app_sfg.urls')),
+    path("", include('app_sfg.urls')),
 ]
