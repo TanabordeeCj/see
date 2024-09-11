2a9fce399746d9b0e50843bdb62f110ace02a62d
TanabordeeCj <tanabordee0313@gmail.com>
2024-09-11 21:01:15 +0700

    update bg

--- app_sfg/urls.py
+++ app_sfg/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.IndexPage, name="index"),
+    path("", views.indexPage, name="index"),
+    path("about/", views.aboutPage, name="about"),
+    path("contact/", views.contactPage, name="contact"),
+
 ]
--- app_sfg/views.py
+++ app_sfg/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def IndexPage(request):
+def indexPage(request):
     return render(request, 'index.html') 
+def aboutPage(request):
+    return render(request, 'about.html') 
+def contactPage(request):
+    return render(request, 'contact.html') 
+
